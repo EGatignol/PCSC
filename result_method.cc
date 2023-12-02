55eb6fc2b132978ecfc0a31790714558750dad10
Enora Virginie Gatignol <gatignol@ma-linux23-02.intranet.epfl.ch>
2023-12-02 10:39:17 +0100

    Add constructor

--- result_method.cc
+++ result_method.cc
@@ -1,4 +1,11 @@
 #include "result_method.hh"
+
+ResultMethod::ResultMethod(std::string Name_method, vector<double> x, std::vector<std::vector<double>> Value_f_by_iteration){
+    NameMethod = Name_method;
+    x_final = x;
+    fvalue = Value_f_by_iteration;
+}
+
 /* -------------------------------------------------------------------------- */
 
 gnuplot ResultMethod::GraphConvergence(){
