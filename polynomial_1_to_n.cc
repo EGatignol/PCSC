2d815648d95671c2377bdc1a57fa4cac96387e88
Enora Virginie Gatignol <gatignol@ma-linux23-02.intranet.epfl.ch>
2023-12-02 10:27:31 +0100

    Implement function Func and DerivedFunc - Problem to access to Eigen

--- polynomial_1_to_n.cc
+++ polynomial_1_to_n.cc
@@ -6,7 +6,15 @@ Polynomial1ToN::Polynomial1ToN(std::vector<std::vector<double>> coeffs, const in
 /* -------------------------------------------------------------------------- */
 
 Eigen::Matrix<double, -1, 1> Polynomial1ToN::Func(std::vector<double> X) {
-
+    Eigen::Matrix<double, dimF, 1> values_f = Eigen::Matrix<double, dimF, 1>::Zero();
+    for (int f_i=0, f_i<=dimF-1, f_i++) {
+        values_f[f_i][0]=0;
+        max_pow_fi = values_f[f_i].size();
+        for (int pow_i=0, pow_i<=max_pow-fi-1, pow_i++ ){
+            values_f[f_i][0]=values_f[f_i][0] + coefficients[f_i][pow_i]*pow(X[0],pow_i);
+        }
+    }
+    return values_f;
 }
 
 
@@ -14,6 +22,15 @@ Eigen::Matrix<double, -1, 1> Polynomial1ToN::Func(std::vector<double> X) {
 /* --------------------------------------------------------------------------- */
 
 Eigen::Matrix<double, -1, -1> Polynomial1ToN::DerivedFunc(std::vector<double> X){
+    Eigen::Matrix<double, dimF, 1> values_f = Eigen::Matrix<double, dimF, 1>::Zero();
+    for (int f_i=0, f_i<=dimF-1, f_i++) {
+        values_f[f_i][0]=0;
+        max_pow_fi = values_f[f_i].size();
+        for (int pow_i=1, pow_i<=max_pow-fi-1, pow_i++) {
+            values_f[f_i][0]=values_f[f_i][0] + coefficients[f_i][pow_i]*pow_i*pow(X[0],pow_i-1);
+        }
+    }
+    return values_f;
 
 }
 
