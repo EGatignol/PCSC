7793ea9a9a55f69c15d4a83cb8b2d36876d0dcab
thibault schiesser <thibault.schiesser@epfl.ch>
2023-12-05 14:36:44 +0100

    ajout exception

--- newton.cc
+++ newton.cc
@@ -10,8 +10,10 @@ Newton::Newton(Eigen::VectorXd x, bool aitken, const double tol, const int maxit
 
 Eigen::VectorXd Newton::NextX(Function &f, Eigen::VectorXd previousX, Eigen::VectorXd previouspreviousX){
     auto facto = f.DerivedFunc(previousX).fullPivLu();
-    std::vector<double> dX = facto.solve(-f.Func(previousX));
-
+    if (facto.isInvertible() == false) {
+        throw std::runtime_error("Singular matrix");
+    }
+    Eigen::VectorXd dX = facto.solve(-f.Func(previousX));
     Eigen::VectorXd newX = previousX + dX;
 
     return newX;
