dc7c50acdc318a1554fee6138f23fc2f610878e9
Enora Virginie Gatignol <gatignol@ma-linux23-09.intranet.epfl.ch>
2023-12-06 11:59:00 +0100

    Fix Error

--- bissection.cc
+++ bissection.cc
@@ -30,12 +30,13 @@ ResultMethod Bissection::MethodFindRoot(Function &F){
     std::vector<Eigen::VectorXd> f_values;
     f_values.reserve(MaxIter);
 
-    if (f_a = 0){
+    auto VectZero = Eigen::VectorXd::Zero(f_a.size());
+    if (f_a == VectZero){
         f_values.push_back(f_a);
         ResultMethod Results(Name, a, f_values);
         return Results;
     }
-    if (f_b = 0){
+    if (f_b == VectZero){
         f_values.push_back(f_b);
         ResultMethod Results(Name, b, f_values);
         return Results;
@@ -47,14 +48,14 @@ ResultMethod Bissection::MethodFindRoot(Function &F){
         m = (a+b)/2;
         f_m = F.Func(m);
         f_values.push_back(f_m);
-        if (f_m = 0){
+        if (f_m == VectZero){
             ResultMethod Results(Name, m, f_values);
             return Results;
         }
-        if (f_a[0][0]*f_m[0][0] < 0){
+        if (f_a[0]*f_m[0] < 0){
             b = m;
         }
-        if (f_b[0][0]*f_m[0][0] < 0){
+        if (f_b[0]*f_m[0] < 0){
             a = m;
         }
         if (abs(a[0]-b[0]) < tolerance){
