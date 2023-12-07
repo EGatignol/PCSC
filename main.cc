88f8096bc020addf94dd880c3b674b9ca5933e06
Enora Virginie Gatignol <gatignol@ma-linux23-09.intranet.epfl.ch>
2023-12-07 15:16:15 +0100

    First function

--- main.cc
+++ main.cc
@@ -2,10 +2,24 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <vector>
+// Files for the function class
+#include "polynomial_1_to_n.hh"
+// Files for the find-root class
+#include "bissection.hh"
+#include "classic_chord.hh"
+#include "fixed_point.hh"
+#include "newton.hh"
+// File for Result_method class
+#include "result_method.hh"
+using namespace std;
 
 int main(int argc, char ** argv){
 
-/// ... your code here ...
+// Define some functions
+// first example: f(x) = x²-1
+std::vector<vector<double>> coeffs_F1 = {{-1, 0, 1}};
+Polynomial1ToN F1(coeffs_F1, 1, 1);
 
   return EXIT_FAILURE;
 }
