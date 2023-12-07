6112548baf035975619c0da206efa1f62d0b800a
Enora Virginie Gatignol <gatignol@ma-linux23-09.intranet.epfl.ch>
2023-12-07 16:34:27 +0100

    Fix an error

--- polynomial_1_to_n.cc
+++ polynomial_1_to_n.cc
@@ -6,7 +6,7 @@ Polynomial1ToN::Polynomial1ToN(std::vector<vector<double>> coeffs, int dim_F, in
 /* -------------------------------------------------------------------------- */
 
 Eigen::VectorXd Polynomial1ToN::Func(Eigen::VectorXd X) {
-    Eigen::VectorXd values_f;
+    Eigen::VectorXd values_f(dimF);
     for (int f_i=0; f_i<=dimF-1; f_i++) {
         values_f[f_i]=0; // Optimal ça?
         int max_pow_fi = coefficients[f_i].size();
@@ -22,7 +22,7 @@ Eigen::VectorXd Polynomial1ToN::Func(Eigen::VectorXd X) {
 /* --------------------------------------------------------------------------- */
 
 Eigen::Matrix<double, -1, -1> Polynomial1ToN::DerivedFunc(Eigen::VectorXd X){
-    Eigen::Matrix<double, -1, 1> values_f;
+    Eigen::Matrix<double, -1, 1> values_f(dimF);
     for (int f_i=0; f_i<=dimF-1; f_i++) {
         values_f[f_i]=0;
         int max_pow_fi = coefficients[f_i].size();
