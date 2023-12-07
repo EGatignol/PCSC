fea2c3033d31763400af555f7cf922087cb336c5
Enora Virginie Gatignol <gatignol@ma-linux23-09.intranet.epfl.ch>
2023-12-07 16:35:33 +0100

    Create a Function and a Method - error during MethodFindRoot()

--- main.cc
+++ main.cc
@@ -2,7 +2,6 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
-#include <vector>
 // Files for the function class
 #include "polynomial_1_to_n.hh"
 // Files for the find-root class
@@ -12,14 +11,29 @@
 #include "newton.hh"
 // File for Result_method class
 #include "result_method.hh"
-using namespace std;
 
 int main(int argc, char ** argv){
 
 // Define some functions
-// first example: f(x) = x²-1
+// first example: F1(x) = x²-1
 std::vector<vector<double>> coeffs_F1 = {{-1, 0, 1}};
 Polynomial1ToN F1(coeffs_F1, 1, 1);
 
+// Methods to find roots for F1
+Eigen::VectorXd X_initial_F1(1);
+X_initial_F1 << 3;
+std::cout << "X initial F1=" << X_initial_F1 << std::endl;
+Newton Newton_F1(X_initial_F1,false, 10e-5,100);
+//std::pair<double,double> Inter_init_F1(0.2, 13.1);
+//Bissection Bissection_F1( Inter_init_F1, 10e-5, 100);
+
+// Results of the method for F1
+ResultMethod Results_Newton_F1 = Newton_F1.MethodFindRoot(F1);
+//std::cout << "Root for Newton_Methods=" << Results_Newton_F1.x_final << std::endl;
+//std::cout << "Values of F1(x)=x2-1 during iteration=";
+//    for (int i = 0; i < Results_Newton_F1.fvalue.size(); ++i) {
+//        std::cout << Results_Newton_F1.fvalue[i] << "";
+//    }
+//    std:cout << std::endl;
   return EXIT_FAILURE;
 }
