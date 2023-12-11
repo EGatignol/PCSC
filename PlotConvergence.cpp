f59f30738d55aa221df6421e6470726449d40bd4
thibault schiesser <thibault.schiesser@epfl.ch>
2023-12-11 16:48:44 +0100

    pas encore testé comme problème avec l'import de gnuplot

--- PlotConvergence.cpp
+++ PlotConvergence.cpp
@@ -1,5 +1,16 @@
 #include "PlotConvergence.h"
-//#include "gnuplot-iostream/gnuplot-isotream.h"
+#include "gnuplot-iostream/gnuplot-iostream.h"
 
-void PlotConvergence::getGraphConv() {
+void PlotConvergence::getGraphConv(){
+    std::vector <double> vnorm;
+    vnorm.reserve(fvalue.size());
+
+    for (const auto& v : fvalue){
+        double norm = v.norm();
+        vnorm.push_back(norm);
+    }
+
+    Gnuplot gp;
+    gp << "plot '-' with linespoints title 'Convergence of the method'\n";
+    gp.send1d(vnorm);
 }
