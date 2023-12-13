a274a88d8cb364b6f37fc2aabd8f8944f32d8db5
Enora Virginie Gatignol <gatignol@ma-linux23-05.intranet.epfl.ch>
2023-12-13 13:34:36 +0100

    Function read

--- CSVReaderPolynome.cpp
+++ CSVReaderPolynome.cpp
@@ -2,19 +2,25 @@
 #include <cassert>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 Polynomial1ToN CSVReaderPolynome::Read(const std::string& NameFile){
     std::ifstream read_file(NameFile);
     assert(read_file.is_open());
-    int dimF;
-    read_file >> dimF;
+
     std::vector<std::vector<double>> coeff;
-    int i=0;
-    while (!read_file.eof())
-    {
-        for(int j=0; j<=dimF-1; j++){
-            read_file >> 
+
+    std::string line;
+    while (std::getline(read_file, line)) {
+        std::istringstream ss(line);
+        std::vector<double> lineFunc;
+        std::string value;
+
+        while (std::getline(ss, value, ',')) {
+            lineFunc.push_back(std::stod(value));
         }
+
+        coeff.push_back(lineFunc);
     }
-    
+    read_file.close();
 }
